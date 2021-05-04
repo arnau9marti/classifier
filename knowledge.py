3806df740d99bd77c647ca6ccdc3cd56c4a08d28
arnau <arnau9marti@gmail.com>
2021-05-04 12:32:17 +0200

    inference com

--- knowledge.py
+++ knowledge.py
@@ -11,34 +11,6 @@ class App:
         # Don't forget to close the driver connection when you are finished with it
         self.driver.close()
 
-    def create_friendship(self, person1_name, person2_name):
-        with self.driver.session() as session:
-            # Write transactions allow the driver to handle retries and transient errors
-            result = session.write_transaction(
-                self._create_and_return_friendship, person1_name, person2_name)
-            for row in result:
-                print("Created friendship between: {p1}, {p2}".format(p1=row['p1'], p2=row['p2']))
-
-    @staticmethod
-    def _create_and_return_friendship(tx, person1_name, person2_name):
-        # To learn more about the Cypher syntax, see https://neo4j.com/docs/cypher-manual/current/
-        # The Reference Card is also a good resource for keywords https://neo4j.com/docs/cypher-refcard/current/
-        query = (
-            "CREATE (p1:Person { name: $person1_name }) "
-            "CREATE (p2:Person { name: $person2_name }) "
-            "CREATE (p1)-[:KNOWS]->(p2) "
-            "RETURN p1, p2"
-        )
-        result = tx.run(query, person1_name=person1_name, person2_name=person2_name)
-        try:
-            return [{"p1": row["p1"]["name"], "p2": row["p2"]["name"]}
-                    for row in result]
-        # Capture any errors along with the query and data for traceability
-        except ServiceUnavailable as exception:
-            logging.error("{query} raised an error: \n {exception}".format(
-                query=query, exception=exception))
-            raise
-
     def find_person(self, person_name):
         with self.driver.session() as session:
             result = session.read_transaction(self._find_and_return_person, person_name)
@@ -110,18 +82,16 @@ if __name__ == "__main__":
     user = "neo4j"
     password = "1234"
     app = App(bolt_url, user, password)
-    #app.create_friendship("Alice", "David")
-    #app.find_person("Alice")
+
     #app.find_topic("artificial intelligence")
-    #app.insert_super_topic("artificial_intelligence") //POT SER AL REVES
+    #app.insert_topic("convolutional_learning", "artificial intelligence")
+
     with open("queries.txt") as f:
         queries = f.readlines()
     queries = [x.strip() for x in queries]
-
-    #app.insert_topic("convolutional_learning", "artificial intelligence")
+    
     for x in queries:
         app.find_topic(x)
-    
     
     app.close()
 
